b8d941b8de025fa5c929a0d58507344a9cbde205
Halil Can KORKMAZ <halilcankorkmaz01@gmail.com>
2024-03-26 02:27:16 +0300

    first commit

---
+++ email-server/server.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const app = express();
+const cors = require('cors');
+app.use(cors());
+const port = process.env.PORT || 3002;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false // Güvenlik riskleri hakkında bilinçli olun
+    }
+});
+
+
+app.post('/send-email', (req, res) => {
+    const { firstname, lastname, email, message } = req.body;
+    const mailOptions = {
+        from: email,
+        to: 'halilcankorkmaz01@gmail.com',
+        subject: `New message from ${firstname} ${lastname}`,
+        text: `You have received a new message from ${firstname} ${lastname}
+        \n\nE-Mail Address: ${email}
+        \n\nMessage:${message}`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+            res.status(500).json({ error: 'Error sending email', detail: error.toString() });
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).json({ message: 'Email sent successfully' });
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
---
+++ js/send-email.js
@@ -0,0 +1,32 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = {
+        firstname: document.getElementById('firstname').value,
+        lastname: document.getElementById('lastname').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
+    };
+
+    fetch('http://localhost:3002/send-email', {        
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok'); // Ağ hatası için
+        }
+        return response.json(); // Bu satır hata fırlatabilir eğer yanıt JSON değilse
+    })
+    .then(data => {
+        console.log('Success:', data);
+        alert('Message sent successfully!');
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert('Error sending message: ' + error.message);
+    });   
+});
